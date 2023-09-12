7b06a1c35fa32a074bdeaa1c4f6c713c53c1ae71
Ghaith <ghaith990000@gmail.com>
2023-09-12 16:44:11 +0300

    Reverse String using stacks

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        FindPivotIndex obj = new FindPivotIndex();
-        int[] gain = {2, 1, -1};
+        StackX theStack = new StackX(10);
+        theStack.push(20);
+        theStack.push(40);
+        theStack.push(60);
+        theStack.push(80);
 
-        System.out.print(obj.pivotIndex(gain));
+        while(!theStack.isEmpty())
+        {
+            long value = theStack.pop();
+            System.out.print(value);
+            System.out.print(" ");
+
+        }
+        System.out.println("");
 
     }
 }
