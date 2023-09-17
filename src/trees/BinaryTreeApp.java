43ea4a2ebed50a683e5bd9194d90bc7ad0e78308
Ghaith <ghaith990000@gmail.com>
2023-09-17 16:45:01 +0300

    add display tree method

--- src/trees/BinaryTreeApp.java
+++ src/trees/BinaryTreeApp.java
@@ -1,5 +1,5 @@
 package trees;
-
+import java.util.Stack;
 class Node {
     public int iData;
     public double dData;
@@ -162,6 +162,96 @@ class Tree {
             successor.rightChild = delNode.rightChild;
         }
         return successor;
+    }
+
+    public void traverse(int traverseType)
+    {
+        switch(traverseType)
+        {
+            case 1: System.out.print("\nPreorder traversal: ");
+                    preOrder(root);
+                    break;
+            case 2: System.out.print("\nInorder traversal: ");
+                    inOrder(root);
+                    break;
+            case 3: System.out.print("\nPostorder traversal: ");
+                    postOrder(root);
+                    break;
+        }
+        System.out.println();
+    }
+
+    private void preOrder(Node localRoot)
+    {
+        if(localRoot != null)
+        {
+            System.out.print(localRoot.iData + " ");
+            preOrder(localRoot.leftChild);
+            preOrder(localRoot.rightChild);
+        }
+    }
+
+    private void inOrder(Node localRoot)
+    {
+        if(localRoot != null)
+        {
+            inOrder(localRoot.leftChild);
+            System.out.print(localRoot.iData + " ");
+            inOrder(localRoot.rightChild);
+        }
+    }
+
+    private void postOrder(Node localRoot)
+    {
+        if(localRoot != null)
+        {
+            postOrder(localRoot.leftChild);
+            postOrder(localRoot.rightChild);
+            System.out.print(localRoot.iData + " ");
+        }
+    }
+
+    public void displayTree(){
+        Stack<Node> globalStack = new Stack<>();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+        System.out.println(
+                ".............................................");
+        while(isRowEmpty==false)
+        {
+            Stack localStack = new Stack();
+            isRowEmpty = true;
+
+            for(int j=0; j<nBlanks; j++)
+                System.out.print(' ');
+
+            while(globalStack.isEmpty()==false)
+            {
+                Node temp = (Node)globalStack.pop();
+                if(temp != null)
+                {
+                    System.out.print(temp.iData);
+                    localStack.push(temp.leftChild);
+                    localStack.push(temp.rightChild);
+
+                    if(temp.leftChild != null || temp.rightChild != null)
+                        isRowEmpty = false;
+                }else {
+                    System.out.print('--');
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for(int j=0; j<nBlanks*2-2; j++)
+                    System.out.print(' ');
+            }
+            System.out.println();
+            nBlanks /= 2;
+            while(localStack.isEmpty()==false)
+                globalStack.push(localStack.pop());
+        }
+        System.out.println(
+                '..................................................................');
     }
 
 }
